9a56fe8d91f18acf5f0f3aa6af9d5ae826e3ca4c
liroma <lilromawaffle@gmail.com>
2026-05-10 01:06:47 +0700

    category bug fix
    -hapus contoh
    -ukuran text titlecat

--- app/src/main/java/com/example/coffeshop/contoh.kt
+++
@@ -1,9 +0,0 @@
-package com.example.coffeshop
-
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import com.example.coffeshop.Domain.CategoryModel
-import com.example.coffeshop.Domain._root_ide_package_.com.example.coffeshop.Domain.CategoryModel
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
